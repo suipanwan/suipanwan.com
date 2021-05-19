15c7be41c645f302856cd5b49390f12526972f98
Wan Sui Pan <suipanwan@gmail.com>
2021-05-20 00:24:13 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app>
+        <v-app-bar
+            app
+            color="blue-grey darken-3"
+            dark
+        >
+            <v-toolbar-title>SUIPANWAN</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+            <v-btn text href="#about">
+                <span class="mr-2">About</span>
+            </v-btn>
+            <v-btn text href="#portfolio">
+                <span class="mr-2">Portfolio</span>
+            </v-btn>
+        </v-app-bar>
+
+        <v-main>
+            <router-view/>
+        </v-main>
+
+        <v-footer dark padless>
+            <!-- Footer-->
+            <v-card
+                id="footer"
+                class="flex"
+                flat
+                tile
+            >
+                <v-card-title class="blue-grey darken-3">
+                    <strong class="subheading">Get connected with us on social networks!</strong>
+
+                    <v-spacer></v-spacer>
+
+                    <v-btn
+                    v-for="icon in icons"
+                    :key="icon"
+                    class="mx-4"
+                    dark
+                    icon
+                    >
+                    <v-icon size="24px">
+                        {{ icon }}
+                    </v-icon>
+                    </v-btn>
+                </v-card-title>
+                <v-card-text class="py-2 white--text text-center">
+                    {{ new Date().getFullYear() }} — <strong>SUIPANWAN.COM</strong>
+                </v-card-text>
+            </v-card>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    name: 'App',
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    data: () => ({
+        icons: [
+            'mdi-facebook',
+            'mdi-twitter',
+            'mdi-linkedin',
+            'mdi-instagram',
+        ],
+    }),
+};
+</script>
---
+++ src/components/AboutMeTimeline.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-container fluid class="pa-0 text-center">
+        <v-timeline
+            align-top
+            :dense="$vuetify.breakpoint.smAndDown"
+        >
+            <v-timeline-item
+                v-for="(item, i) in items"
+                :key="i"
+                :color="item.color"
+                :icon="item.icon"
+                fill-dot
+                >
+                <span v-if="item.opposite_text" slot="opposite">{{item.opposite_text}}</span>
+                <v-card
+                    :color="item.color"
+                    dark
+                >
+                    <v-card-title class="title">
+                    {{item.title}}
+                    </v-card-title>
+                    <v-card-text v-if="item.content" class="white text--primary">
+                    <p>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.</p>
+                    </v-card-text>
+                </v-card>
+            </v-timeline-item>
+        </v-timeline>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data: () => ({
+      items: [
+        {
+          color: 'red lighten-2',
+          icon: 'mdi-star',
+          opposite_text: '2013',
+          title: 'Graduated from Secondary School',
+          content: ''
+        },
+        {
+          color: 'purple darken-1',
+          icon: 'mdi-book-variant',
+        },
+        {
+          color: 'green lighten-1',
+          icon: 'mdi-airballoon',
+        },
+        {
+          color: 'indigo',
+          icon: 'mdi-buffer',
+        },
+      ],
+    }),
+}
+</script>
